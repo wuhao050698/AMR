a386f6d3e71a767749f02333382fdbc93c7f95d5
wuhao050698 <l>
2020-07-23 21:44:22 +0800

    a new translator

--- translator.py
+++ translator.py
@@ -1,74 +1,96 @@
+# to find the next key char in the AMR
+def find_min(str):
+    ans = len(str)+1
+    if str.find('(')!=-1:
+        ans = min(str.index('('),ans)
+    if str.find(')')!=-1:
+        ans = min(str.index(')'),ans)
+    if str.find('/')!=-1:
+        ans = min(str.index('/'),ans)
+    if str.find(':')!=-1:
+        ans = min(str.index(':'),ans)
+    if ans == len(str)+1:
+        return 0
+    else:
+        return ans
+
+# the translator from AMR text format to conjunction of triples
 def translator(input,output):
     # read the dataset
     with open(input, 'r', encoding='utf8') as load:
         loads = load.readlines()
-    h = 0
-    cnt = 0
-    instance = []  # the instance
-    arg = []  # the edge
-    judge = []  # judge the same instance
-    last_rank = 0  # Determine the number of layers of the current node according to the brackets
-    last_var = ''  # last variable
-    father = {}  # Record the parent node of each layer
 
-    # process
+    amr = ''
     for line in loads:
-
-        if line == '\n':
-            h = 0
-            cnt = 0
-            instance = []  # the instance
-            arg = []  # the edge
-            judge = []  # judge the same instance
-            last_rank = 0  # Determine the number of layers of the current node according to the brackets
-            last_var = ''  # last variable
-            father = {}  # Record the parent node of each layer
-            continue
         if line[0] == '#':
             continue
 
+        # process one AMR
+        if line == '\n' and amr != '':
+            instance = []  # the instance
+            arg = []  # the relation
+            stack = []  # store each layer's instance in the stack judged by the brackets
+            while 1:
+                # if the key char is ( ,push the word into the stack
+                if amr[0] == '(':
+                    amr = amr[1:]
+                    temp = find_min(amr)
+                    word = amr[0:temp]
+                    word = word.strip()
+                    amr = amr[temp:]
+                    stack.append(word)
+                # if the key char is ), pop the word from the stack
+                elif amr[0] == ')':
+                    stack.pop()
+                    amr = amr[1:]
+                # if the key char is /, store the words on the both sides of it in the instance
+                elif amr[0] == '/':
+                    amr = amr[1:]
+                    temp = find_min(amr)
+                    word = amr[0:temp]
+                    word = word.strip()
+                    amr = amr[temp:]
+                    instance.append({'var': stack[-1], 'instance': word})
 
-        # preprocess
-        h += 1
-        flag = 0
-        line = line.strip('\n')
-        rank=line.count('(')-line.count(')')+last_rank  # judge its layer by the buckets
-        line = line.replace('(', '')
-        line = line.replace(')', '')
-        line = line.replace('/ ', '')
-        line = line.replace(':', '')
-        line = line.strip()
-        s_line = line.split(' ')
+                # if the key char is :
+                elif amr[0] == ':':
+                    flag = 1
+                    amr = amr[1:]
+                    temp = find_min(amr)
+                    judge = amr.find(' ')
+                    # if the word is not a instance
+                    if judge < temp and amr[judge + 1] != '(':
+                        flag = 0
+                        rel = amr[0:judge]
+                        rel.strip()
+                        amr = amr[judge:]
+                    else:  # if the word is a instance
+                        rel = amr[0:temp]
+                        rel.strip()
+                        amr = amr[temp:]
 
-        if h == 1:  # the root
-            instance.append({'var': s_line[0], 'instance': s_line[1]})
-            judge.append(s_line[0])
-            last_rank = rank
-            last_var = s_line[0]
-            continue
-        else:
-            if len(s_line) == 2:
-                if s_line[1] not in judge:
-                    judge.append(s_line[1])
-            else:
-                instance.append({'var': s_line[1], 'instance': s_line[2]})
-                judge.append(s_line[1])
-        # if it's a new laywer, store its father
-        if last_rank not in father.keys():
-            father[last_rank] = last_var
-        last_var = s_line[1]
-        arg.append({'rel': s_line[0], 'a': father[last_rank], 'b': s_line[1]})
-        last_rank = rank
-
-        # If the AMR has ended, store the generated triples in a file
-        if last_rank==0:
+                    amr = amr[1:]
+                    temp = find_min(amr)
+                    word = amr[0:temp]
+                    word = word.strip()
+                    amr = amr[temp:]
+                    arg.append({'rel': rel.strip(), 'a': stack[-1], 'b': word})  # store the relation
+                    if flag == 1:
+                        stack.append(word)  # if the word is a instance, push it into the stack
+                if amr == '':  # end
+                    break
+                if amr[0] == ' ':
+                    amr = amr[1:]
             with open(output, 'a', encoding='utf8') as fo:
-                for i in instance:  # the instance
+                for i in instance:
                     fo.write("instance(" + i['var'] + "," + i['instance'] + ")" + "\n")
-                for i in arg:  # the arg
+                for i in arg:
                     fo.write(i['rel'] + "(" + i['a'] + "," + i['b'] + ")" + "\n")
                 fo.write("\n")
-            continue
+
+        line = line.strip()
+        line = line.strip('\n')
+        amr += line
 
 
 if __name__ == '__main__':
